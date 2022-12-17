90fea45d048d79f52d235e7298cb79dfa2369ec7
Jake <jakepricep99@gmail.com>
2022-12-16 21:29:18 -0600

    necessary files

---
+++ src/main/java/org/example/GameWindow.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    private JFrame jFrame;
+
+    public GameWindow(GamePanel gamePanel) {
+        jFrame = new JFrame();
+        jFrame.setSize(400, 400);
+        jFrame.add(gamePanel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
