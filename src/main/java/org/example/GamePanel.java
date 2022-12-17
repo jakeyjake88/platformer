42d03b5d712578d51c796f155c9f0908c6a794d8
Jake <jakepricep99@gmail.com>
2022-12-17 01:05:37 -0600

    more stuff

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -5,25 +5,24 @@ import inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyListener;
-import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private float xDelta = 100;
     private float yDelta = 100;
-    private float xDir = 1f;
-    private float yDir = 1f;
-    private int frames = 0;
-    private long lastCheck = 0;
-    private Color color = new Color(150, 20, 90);
-    private Random random;
     public GamePanel() {
-        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
+        setPanelSize();
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
 
     public void changeXDelta(int value) {
@@ -39,39 +38,10 @@ public class GamePanel extends JPanel {
 
     }
 
-    public void setRectPosition(int x, int y) {
-        this.xDelta = x;
-        this.yDelta = y;
-        repaint();
-    }
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        updateRect();
-        g.setColor(color);
-        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
         Toolkit.getDefaultToolkit().sync();
 
 
-    }
-
-    private void updateRect() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir*=-1;
-            color = getRndColor();
-        }
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir*= -1;
-            color = getRndColor();
-        }
-    }
-
-    private Color getRndColor() {
-        int r = random.nextInt(255);
-        int b = random.nextInt(255);
-        int g = random.nextInt(255);
-        return new Color(r, b, g);
     }
 }
--- src/main/java/org/example/GameWindow.java
+++ src/main/java/org/example/GameWindow.java
@@ -1,22 +1,17 @@
 package org.example;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class GameWindow extends JFrame {
     private JFrame jFrame;
 
     public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
-        setPanelSize();
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.pack();
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    private void setPanelSize() {
-        Dimension size = new Dimension(1280, 800);
-        setMinimumSize()
     }
 }
