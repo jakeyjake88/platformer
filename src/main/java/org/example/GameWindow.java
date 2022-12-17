836c40d283a9c0cdb952818bf52869f6b5910cfd
Jake <jakepricep99@gmail.com>
2022-12-17 01:01:16 -0600

    more stuff

--- src/main/java/org/example/GameWindow.java
+++ src/main/java/org/example/GameWindow.java
@@ -1,16 +1,22 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow extends JFrame {
     private JFrame jFrame;
 
     public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
-        jFrame.setSize(400, 400);
+        setPanelSize();
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize()
     }
 }
